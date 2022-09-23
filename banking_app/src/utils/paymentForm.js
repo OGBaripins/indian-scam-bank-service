cc09889214f157c2da8e7e6ce768373f38c2bc26
markusvilcans <markusvilcans@gmail.com>
2022-09-23 14:44:01 +0300

    lauki auto-filled no json

--- banking_app/src/utils/paymentForm.js
+++ banking_app/src/utils/paymentForm.js
@@ -1,6 +1,7 @@
 import React, {useState } from 'react'
 import {motion} from 'framer-motion/dist/framer-motion'
 import PopMessage from '../utils/popMessage';
+import jsonData from './data.json';
 function paymentForm(props) {
     let user = props
     const [isNotEnough, setIsNotEnough] = useState(false)
@@ -8,22 +9,22 @@ function paymentForm(props) {
   return (
     <div className='formBox z4'>
         <form className='formStyle'>
-
+            
             <label className='formlabelBox'>
                 <h4 className='label'>Reciver name</h4>
-                <input type="text" className='inputBox'/>
+                <input type="text" className='inputBox' value={jsonData.name} disabled/>
             </label>
             <label className='formlabelBox'>
                 <h4 className='label'>Reciver account</h4>
-                <input type="text" className='inputBox'/>
+                <input type="text" className='inputBox' value={jsonData.account} disabled/>
             </label>
             <label className='formlabelBox'>
                 <h4 className='label'>Amount</h4>
-                <input type="number" className='inputBox'/>
+                <input type="number" className='inputBox' value={jsonData.amount} disabled/>
             </label>
-            <label className='formlabelBox'>
+            <label className='formlabelBox' >
                 <h4 className='label'>Description</h4>
-                <input type="text" className='inputBox'/>
+                <input type="text" className='inputBox' value={jsonData.desc} disabled/>
             </label>
             <motion.button className='submit' 
             type="button"
