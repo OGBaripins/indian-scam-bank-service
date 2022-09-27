255fab6ab7a30c459c8c5357410879d65af37231
AleksisVorobjovs <aleksis371@inbox.lv>
2022-09-27 17:22:37 +0300

    naw good call rederect monkey work monkey happy

--- banking_app/src/utils/loginForm.js
+++ banking_app/src/utils/loginForm.js
@@ -1,33 +1,39 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+import { useNavigate } from "react-router-dom";
 
 const loginForm = ({paymentInfo, goodLink, badLink}) => {
 
-  const adminUser = {
-    number: "010101-11111",
-    password: "password1"
-  }
+  const navigate = useNavigate();
 
-  const [user, setUser] = useState({number:""});
-  const [error, setError] = useState("");
+  let user;
+
   const [details, setDetails] = useState({number:"",password:""});
-
+  
   const submitHandler = (e) =>{
-    axios.get(`http://127.0.0.1:5000/validation/${details.number}/${details.password}`)
+    e.preventDefault();
+  axios({
+    method: "get",
+    url: `http://127.0.0.1:5000/validation/${details.number}/${details.password}`,
+  })
     .then(function (response) {
-      console.log(response);
+      if(details.number == response.data[0].social_security_number){
+        user = response.data[0];
+        navigate('/',{
+          state: {
+            user: {user},
+            paymentInfo: {paymentInfo}, 
+            goodLink: {goodLink}, 
+            badLink: {badLink}
+          }
+        })
+      }
     })
-    .catch(function (error) {
-      console.log(error);
+    .catch(function (response) {
+      //handle error
+      console.log(response);
     });
-    if(details.number == adminUser.number && details.password == adminUser.password){
-      setUser({number: details.number});
-      setError("");
-    }else{
-      e.preventDefault();
-      setError("Details do not match");
-    }
   }
 
   return (
@@ -41,7 +47,7 @@ const loginForm = ({paymentInfo, goodLink, badLink}) => {
             <h4 className='label z4'>Password</h4>
             <input className='inputBox z4' type="password" name="password" id="password" onChange={e => setDetails({...details, password: e.target.value})} value={details.password}/>
         </label>
-        <Link to="/" className='submit z4' onClick={submitHandler} paymentInfo={paymentInfo} goodLink={goodLink} badLink={badLink}>
+        <Link to="/" className='submit z4' onClick={submitHandler}>
             <h4 className='submitText z4'>Login</h4>
         </Link>
     </form>
--- banking_app/src/views/mainForm.js
+++ banking_app/src/views/mainForm.js
@@ -2,14 +2,14 @@ import React from 'react'
 import '../App.css';
 import PaymentForm from '../utils/paymentForm';
 import { Link } from "react-router-dom";
-import anime from '../images/anime.mp4'
+import anime from '../images/anime.mp4';
+import { useLocation } from 'react-router-dom';
 
-let user = {
-  "name":"test",
-  "surname":"test",
-  "amount":"850"
-}
+let user;
 const mainForm = () => {
+  const location = useLocation();
+  user=location.state.user.user;
+  console.log(location);
   return (
     <div className='contentBox'>
         <video className='videoBack' autoPlay loop mute>
@@ -19,9 +19,9 @@ const mainForm = () => {
         <PaymentForm user={user}/>
         <div className='accountBox'>
             <h4 className='smallTitle'>LVHABA21381723812</h4>
-            <h4 className='infoText'>Name: {user.name}</h4>
-            <h4 className='infoText'>Surname: {user.surname}</h4>
-            <h4 className='infoText'>Amount: {user.amount}$</h4>
+            <h4 className='infoText'>Name: {user.first_name}</h4>
+            <h4 className='infoText'>Surname: {user.last_name}</h4>
+            <h4 className='infoText'>Balance: {user.balance}$</h4>
         </div>
     </div>
   )
